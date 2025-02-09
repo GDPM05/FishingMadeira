0ac77e1b2aa177f98af4f9854417a2a3500eb289
Vítor Rodrigues <vrodric7@gmail.com>
2025-02-09 16:19:30 +0000

    Optimized SEO, Setup view building with dinamic css and js loading and logging

--- app/Controllers/Authentication.php
+++ app/Controllers/Authentication.php
@@ -6,6 +6,6 @@ class Authentication extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return $this->buildViews("login_view");
     }
 }
