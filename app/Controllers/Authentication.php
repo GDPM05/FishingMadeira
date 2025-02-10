f2a2728d8538ecbe0d40957c2e084f9cca40add6
Vítor Rodrigues <vrodric7@gmail.com>
2025-02-10 14:43:56 +0000

    ...

--- app/Controllers/Authentication.php
+++ app/Controllers/Authentication.php
@@ -6,6 +6,7 @@ class Authentication extends BaseController
 {
     public function index(): string
     {
+        $this->data['title'] = "Fishing Madeira - Login";
         return $this->buildViews("login_view");
     }
 }
