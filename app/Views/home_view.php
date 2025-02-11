c44619391eafc1889a3bc275da6bfe1d2632db79
= <j.p.f.a009@gmail.com>
2025-02-11 14:07:28 +0000

    Minimal changes

---
+++ app/Views/home_view.php
@@ -0,0 +1,21 @@
+<div class="container main-content">
+    <div class="row">
+        <div class="col-6"><p><b>Fishing Madeira</b></p></div>
+        <div class="col-6"><a href="#">Join</a></div>
+    </div>
+    <div class="row">
+        <div class="col-8 left-content">
+            <div class="row">
+            <div class="col-12"><h1>Reel in connections</h1></div>
+            </div>
+            <div class="row">
+                <div class="col-12"><h2>Share your fishing adventures</h2></div>
+            </div>
+        </div>
+        <div class="col-4 right-content">
+            <div class="image-container">
+                <img src="<?php echo base_url($right_side_image)?>" alt="Fishing Image">
+            </div>
+        </div>
+    </div>
+</div>
